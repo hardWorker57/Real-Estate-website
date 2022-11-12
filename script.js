a7a8d71d7008b088672b984d4a95ff87e7a42e7d
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2022-11-13 00:53:03 +0600

    origin-beta

---
+++ script.js
@@ -0,0 +1,16 @@
+// function showName() {
+//     console.log('Vasya!');
+// }
+// setTimeout(showName, 0);
+// console.log('Kolya!')
+
+// showMessage();
+// function showMessage() {
+//     console.log("Message")
+// }
+
+// let showMessage = function () {
+//     console.log("Message")
+// }
+// showMessage();
+
